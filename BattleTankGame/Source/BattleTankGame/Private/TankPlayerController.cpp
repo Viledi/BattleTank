fa8e2207a59d85b79e8b9c66a2c68b9cd99e503f
Vika <viktoriaover@gmail.com>
2019-02-04 11:29:14 -0800

    BP14 Created C++ TankPlayerController

---
+++ BattleTankGame/Source/BattleTankGame/Private/TankPlayerController.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Public/TankPlayerController.h"
+
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay(); 
+
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController not possesing a tank"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("%s is controlled."), *ControlledTank->GetName());
+	}
+
+	UE_LOG(LogTemp, Warning, TEXT("PlayerController Begin Play"));
+
+
+}
+
+ATank* ATankPlayerController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
